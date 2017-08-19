6ec484f9c5cb8e3ca128f0faf0c1800a690a1441
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-08-19 17:45:44 +0200

    lol

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,11 @@
 <footer>
 	<div class="footer_contact">
 	    <h2>Contacts :</h1>
+		<!-- LOL
 		<p><a href="mailto:antoine.artillan@telecomnancy.net" title="Un apprenti webmaster">antoine.artillan@telecomnancy.net</a></p>
 		<p><a href="mailto:benoit.frapiccini@telecomnancy.net" title="Un platypus">benoit.frapiccini@telecomnancy.net</a></p>
+		-->
+		<p><a href="http://media.boingboing.net/wp-content/uploads/2013/01/2013-01-17-1730421.jpg" title="Un apprenti webmaster">antoine.artillan@telecomnancy.net</a></p>
+		<a href="http://1.bp.blogspot.com/-HlErp9h3rhM/T3HwAMuW_lI/AAAAAAAA-FU/Dv-hxc92wQU/s1600/Adorable+Photos+Of++Puggles+(Baby+Platypuses)+(12).jpg" title="Un platypus">benoit.frapiccini@telecomnancy.net</a></
 	</div>
 </footer>
