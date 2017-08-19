8e9ed59035d6bcca01edc924e3698a86027e31d8
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-08-19 15:01:48 +0200

    debut du login

---
+++ components/navigation.php
@@ -0,0 +1,6 @@
+<nav>
+	<p><a href="wiki/mainwiki.html" title="Pour en apprendre plus sur ce monde fantastique !">Wiki</a></p>
+	<p><a href="forum.html" title="Pour partager vos expériences avec d'autres aventuriers !">Forums</a></p>
+	<p><a href="communauté.html" title="Pour découvrir les derniers fan arts !">Communauté</a></p>
+	<p><a href="Login.html" title="T'es qui au juste ?">Login</a></p>
+</nav>
--- extract/bannière.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <ul>
-		<p><a href="wiki/mainwiki.html" title="Pour en apprendre plus sur ce monde fantastique !">Wiki</a></p>
-		<p><a href="forum.html" title="Pour partager vos expériences avec d'autres aventuriers !">Forums</a></p>
-		<p><a href="communauté.html" title="Pour découvrir les derniers fan arts !">Communauté</a></p>
-	</ul>
-</nav>
