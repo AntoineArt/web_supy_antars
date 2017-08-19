b9d3fcddf70fe95ccff394863df0834f972ef222
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-08-19 21:13:16 +0200

    maj

--- components/header.php
+++ components/header.php
@@ -1,8 +1,8 @@
 <header>
 	<div class="titre">
 	    <h1>
-			World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
-			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃
+	    	<a href="../_mainpage" title="World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic,マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃"> World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
+			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃 </a>
 		</h1>
 	</div>
 </header>
