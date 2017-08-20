ecfdfd3ef1f614048ff02c6277dd832c9ed8bf41
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-08-20 17:45:16 +0200

    maj + travail sur le login

--- components/header.php
+++ components/header.php
@@ -1,8 +1,8 @@
 <header>
 	<div class="titre">
 	    <h1>
-	    	<a href="../_mainpage" title="World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic,マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃"> World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
-			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃 </a>
+	    	World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
+			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃
 		</h1>
 	</div>
 </header>
--- login/login.php
+++ login/login.php
@@ -1,19 +1,52 @@
-<!DOCTYPE html>
+<?php
+session_start();
+if (isset($_SESSION['identifiant'])){
+	setcookie('pseudo', $_SESSION['identifiant'], time() + 3600, null, null, false, true); // Cookie :D
+}
+?>
 
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<link rel="stylesheet" href="../stylesheet.css" />
+		<link rel="stylesheet" href="../stylesheet.css"  />
 		<title>Login</title>
 	</head>
 
 	<body>
 		<div id="page">
-			<form action="_mainpage.php" method="post">
-				<p> Identifiant <input type="text" name="pseudo" /> </p>
-		    	<p> Mot de passe <input type="password" name="mdp" /> </p>
-		    	<P> <input type="submit" value="Valider" /> </p>
-			</form>
+
+			<?php
+			if (isset($_POST['identifiant']) AND $_POST['identifiant'] ==  "supytalp" AND $_POST['mdp'] AND $_POST['mdp'] ==  "platypus")
+    		//LOGGED
+    		{
+    		?>
+    			<h1>UN PLATYPUS SAUVAGE APPARAIT !</h1>
+    			<figure>
+    				<img src="../images/wartz.jpeg" alt="o3o" />
+				</figure>
+    			<?php
+    		}
+
+    		else
+    		//NOT LOGGED
+    		{
+    		?>
+				<!--HEADER-->
+			    <?php include("../components/header.php"); ?>
+
+		        <!--MAIN-->
+		        <section>
+					<form action="login.php" method="post">
+						<h1>Connexion :</h1>
+						<p> Identifiant <input type="text" name="identifiant" /> </p>
+				    	<p> Mot de passe <input type="password" name="mdp" /> </p>
+				    	<P> <input type="submit" value="Valider" /> </p>
+					</form>
+				</section>
+				<?php
+			} ?>
+
 		</div>
 	</body>
 </html>
