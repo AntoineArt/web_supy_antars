64fcf246cfda85dd9b193f042a6527d0e69a5d9b
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-08-26 08:29:57 +0200

    ajout d'un lien sur le titre; modification du css en conséquence

--- site/vue/components/header.php
+++ site/vue/components/header.php
@@ -1,8 +1,13 @@
+<!--
+	Auteur: Frapiccini Benoît
+	Ce code html correspond à l'en-tête de la majorité des pages.
+-->
+
 <header>
 	<div class="titre">
 	    <h1>
-	    	World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
-			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃
+	    	<a href="_main.php?section=mainpage">World Of Magical Monster Evangelion Hunter Fortress VS Attack on Titans of Might and Magic<br />
+			マジカルモンスターエヴァンゲリオンハンター砦の世界VSタイトな騎士と魔法の攻撃</a>
 		</h1>
 	</div>
 </header>
