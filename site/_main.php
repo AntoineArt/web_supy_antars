3d5d0b597ac98442ab30212b3b8702e41758e33f
Benoît Frapiccini <benoit.frapiccini@telecomnancy.net>
2017-09-02 16:04:28 +0200

    correction de bug

--- site/_main.php
+++ site/_main.php
@@ -15,66 +15,68 @@
 	$_SESSION['error']=0;
 	include_once('controleur/identification/autoconnect.php');
 
-	// On regarde d'abord si on est renvoyé sur une section dynamique
-	if($_GET['section'] == 'dynamic_section'){
-		$section = $_SESSION['dynamic_section'];
-	}
-	// On regarde ensuite si l'utilisateur est nouveau
-	elseif(!isset($_GET['section'])){
+	// On regarde d'abord si l'utilisateur est nouveau
+	if(!isset($_GET['section'])){
 		include_once('vue/mainpage/mainpage.php');
 	}
-	// Sinon, on utilise la redirection de l'URL
 	else{
-		$section = $_GET['section'];
-	}
+	// On regarde ensuite si on est renvoyé sur une section dynamique
+		if($_GET['section'] == 'dynamic_section'){
+			$section = $_SESSION['dynamic_section'];
+		}
+	// Sinon, on utilise la redirection de l'URL
+		else{
+			$section = $_GET['section'];
+		}
 
-	switch ($section)
-	{
+		switch ($section)
+		{
 		// Mainpage
-		case 'mainpage':
-		include_once('vue/mainpage/mainpage.php');
-		break;
+			case 'mainpage':
+			include_once('vue/mainpage/mainpage.php');
+			break;
 
 		// News
-		case 'mainnews':
-		break;
+			case 'mainnews':
+			break;
 
 		// Wiki
-		case 'mainwiki':
-		include_once('vue/wiki/mainwiki.html');
-		break;
+			case 'mainwiki':
+			include_once('vue/wiki/mainwiki.html');
+			break;
 
 		// Forums
-		case 'mainforums':
-		include_once('controleur/forums/index.php');
-		break;
+			case 'mainforums':
+			include_once('controleur/forums/index.php');
+			break;
 
-		case 'nouveau_billet':
-		include_once('controleur/forums/nouveau_billet.php');
-		break;
+			case 'nouveau_billet':
+			include_once('controleur/forums/nouveau_billet.php');
+			break;
 
-		case 'commentaires':
-		include_once('controleur/forums/commentaires.php');
-		break;
+			case 'commentaires':
+			include_once('controleur/forums/commentaires.php');
+			break;
 
-		case 'nouveau_commentaire':
-		include_once('controleur/forums/nouveau_commentaire.php');
-		break;
+			case 'nouveau_commentaire':
+			include_once('controleur/forums/nouveau_commentaire.php');
+			break;
 
 		// Identification
-		case 'connexion':
-		include_once('controleur/identification/connexion.php');
-		break;
+			case 'connexion':
+			include_once('controleur/identification/connexion.php');
+			break;
 
-		case 'deconnexion':
-		include_once('controleur/identification/deconnexion.php');
-		break;
+			case 'deconnexion':
+			include_once('controleur/identification/deconnexion.php');
+			break;
 
-		case 'inscription':
-		include_once('controleur/identification/inscription.php');
-		break;
+			case 'inscription':
+			include_once('controleur/identification/inscription.php');
+			break;
 
 		// Rip
-		default:
-		echo ('Sorry, nothing to see here.');
+			default:
+			echo ('Sorry, nothing to see here.');
+		}
 	}
